b9ee0f7fdbe09502d0be1d3b962a691d2d0b0cb5
pisces2336 <pisces2336@gmail.com>
2023-11-24 21:24:33 +0900

    update: 言語,アンダースコア数,ハッシュ文字数の選択を廃止

--- script.js
+++ script.js
@@ -1,44 +1,16 @@
 const input = document.getElementById('js_input');
 const output = document.getElementById('js_output');
-
-const languages = document.getElementsByName('language');
-const lowerSelecter = document.getElementById('js_underscoreLowerSelecter');
-const upperSelecter = document.getElementById('js_underscoreUpperSelecter');
-const numHashSlecter = document.getElementById('js_numHashSelecter');
-
 const trigger = document.getElementById('js_trigger');
-
-const marksObj = {
-  'html': ' "',
-  'css': ' ,.#[>~+|:',
-};
+const marks = ' ",.#[>~+|:';
+const regBase = '__.{5}\\';
 
 trigger.addEventListener('click', () => {
-  let language = '';
-  languages.forEach(lang => {
-    if (lang.checked) {
-      language = lang.value;
-    }
-  });
-
-  const lowerIdx = lowerSelecter.selectedIndex;
-  const lower = lowerSelecter.options[lowerIdx].value;
-
-  const upperIdx = upperSelecter.selectedIndex;
-  const upper = upperSelecter.options[upperIdx].value;
-
-  const numHashIdx = numHashSlecter.selectedIndex;
-  const numHash = numHashSlecter.options[numHashIdx].value;
-
-  const regBase = `_{${lower},${upper}}.{${numHash}}\\`;
-
   let str = input.value;
-  const marks = marksObj[language];
   Array.prototype.forEach.call(marks, mark => {
     let reg = new RegExp(regBase + mark, 'g');
-    console.log(reg);
     str = str.replace(reg, mark);
+    console.log(reg);
+    console.log(str);
   });
-
   output.value = str;
 });
